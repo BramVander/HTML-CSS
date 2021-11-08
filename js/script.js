28649745da9f06f73a2150397e6744a80713bb1a
Vander <bram1105@hotmail.com>
2021-11-09 00:50:14 +0100

    footer

--- js/script.js
+++ js/script.js
@@ -2,20 +2,28 @@
 const hamburger = document.querySelector("#hamburger-icon");
 const navLinks = document.querySelector(".nav-links");
 
+// Logo click for homepage
+const goHome = function () {
+  console.log("go home");
+  window.location.href = "#";
+};
+
 // Open / close hamburger menu
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("open");
 });
 
-// Logo click for homepage
-const goHome = function () {
-  window.location.href = "#";
-};
-
 // Toggle class for current nav link
 navLinks.addEventListener("click", function (e) {
   e.preventDefault();
-  let active = document.querySelector(".current-page");
+  const active = document.querySelector(".current-page");
   if (active) active.classList.remove("current-page");
   e.target.classList.add("current-page");
+
+  // Prevent click on parent
+  if (!e.target.getAttribute("href")) return;
+
+  // Travel to clicked navlink
+  const slug = e.target.getAttribute("href");
+  window.location.href = `${slug}`;
 });
