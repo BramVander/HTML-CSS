ad5e03dd639ae73ff69be14ac40d047e05f3c04a
Vander <bram1105@hotmail.com>
2021-11-10 16:55:18 +0100

    smallfix navbar

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 // Store elements
 const hamburger = document.querySelector("#hamburger-icon");
+const mobileMenu = document.querySelector(".mobile-menu");
 const navLinks = document.querySelector(".nav-links");
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector(".body");
 
 // Logo click for homepage
 const goHome = function () {
-  console.log("go home");
   window.location.href = "#";
 };
 
 // Toggle hamburger menu
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("open");
+  mobileMenu.classList.toggle("open");
+  overlay.classList.toggle("open");
+  body.classList.toggle("no-scroll");
 });
 
 // Toggle class for current nav link
 navLinks.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("click");
   const active = document.querySelector(".current-page");
   if (active) active.classList.remove("current-page");
   e.target.classList.add("current-page");
